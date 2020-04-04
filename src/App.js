8f0472655660206c1288c50fba6b01a23c2f2da6
Leonardo <leonardowlach@gmail.com>
2020-04-04 20:14:19 -0300

    change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Fuck yeah baby!
+          Fuck yeah baby! Now on AWS automatically!
         </p>
         <a
           className="App-link"
